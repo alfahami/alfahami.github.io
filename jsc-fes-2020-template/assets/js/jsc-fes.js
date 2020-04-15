ed54e0215a237aa4ffa88f920852bf22c538561e
AL-FAHAMI Toihir <altoihir@gmail.com>
2020-04-15 22:49:07 +0200

    Update jsc-fes.js

--- jsc-fes-2020-template/assets/js/jsc-fes.js
+++ jsc-fes-2020-template/assets/js/jsc-fes.js
@@ -173,7 +173,7 @@
 $("#countdown").countdown(
   {
     //The countdown end date
-    date: "13 April 2020 00:00:00",
+    date: "20 September 2020 00:00:00",
 
     // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
     format: "on"
